1580360ac0bc7692bc129ba6aef9d0f64d0bfa51
Tusher37 <ttusherdebnath@gmail.com>
2024-12-07 19:55:58 +0600

    Home & About section made mobile responsive

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,16 +1,13 @@
 
 const About = () => {
     return (
-        <section >
-            <div className="m-10">
+            <div className="mt-[590px] lg:mt-0 m-10">
                 <h1 id="about" className="text-5xl text-indigo-500 font-semibold pt-12">About Me</h1>
-                <div className="flex gap-5 mt-3">
-                    <div className="w-3 h-[142px] bg-white"> </div>
+                <div className="flex gap-5 mt-6 lg:mt-3">
+                    <div className="w-3 h-[510px] lg:h-[142px] bg-white"> </div>
                     <p className="text-lg text-justify">I am a passionate and dedicated web developer with a solid foundation in computer science and engineering, recently graduated from Independent University, Bangladesh. With hands-on experience in front-end development, I excel in creating responsive and user-friendly applications using HTML5, CSS3, JavaScript, React, and Tailwind CSS. My academic background CSE, combined with practical experience, showcases my ability to build innovative web solutions. Driven by analytical thinking, effective communication, and a commitment to continual learning, I am eager to contribute my skills and grow within a dynamic professional environment.</p>
-
                 </div>
             </div>
-        </section>
     );
 };
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,6 @@ import location from '../assets/location.gif';
 import bdFlag from '../assets/bangladesh_flag_lottie_json_animation_800.gif';
 const Home = () => {
     return (
-        
             <div id="home" className='m-6 grid grid-cols-1 gap-1 h-[500px] w-[90vw] items-center justify-center bg-slate-950 text-white'>
                 <div className="flex flex-col lg:flex-row justify-center lg:justify-between pt-10">
                     <div className="card-wrapper h[600px] lg:h-[250px] w-[350px] lg:w-[950px]">
@@ -20,62 +19,62 @@ const Home = () => {
                                 <p className="text-4xl text-end text-indigo-400 font-semibold pb-1">I'm Tusher Debnath</p>
                             </div>
                             <div className="m-5">
-                                <div className="flex gap-1 pb-5">
-                                    <p className="text-3xl text-amber-100 pt-5">|  Web Developer </p>
+                                <div className="flex gap-5 lg:gap-2 pb-5">
+                                    <p className="text-2xl lg:text-3xl text-amber-100 pt-5">|  Web Developer </p>
                                     <img className="h-[100px] w-[100px]" src={webImg} alt="" />
                                 </div>
                                 {/* <div  className="flex">
                                     <p className="text-3xl text-amber-100 pb-6">| Researcher</p>
                                     <img className="h-[60px] w-[60px]" src="/src/assets/researcher.gif" alt="" />
                                 </div> */}
-                                <div className="flex gap-4">
-                                    <p className="text-3xl text-amber-100 pt-5">| Tech Enthusiast</p>
+                                <div className="flex gap-6 lg:gap-4">
+                                    <p className="text-2xl lg:text-3xl text-amber-100 pt-5">| Tech Enthusiast</p>
                                     <img className="h-[80px] w-[90px]" src={techImg} alt="" />
-                                </div>                                
-                                
+                                </div>                                                               
                             </div>                   
                         </div>
                     </div> 
-                    <div className="flex space-x-4 gap-8  h-[250px]">
+                    <div className="flex space-x-4 gap-8  h-[250px] justify-center pt-3 lg:pt-0">
                         <div className="card-wrapper h-[250px] rounded-xl">
-                            <img className="card-content h-[250px] w-[350px] lg:w-[270px] rounded-xl" src={myPic} alt="" />
+                            <img className="card-content h-[250px] w-[550px] lg:w-[270px] rounded-xl" src={myPic} alt="" />
                         </div>   
                     </div> 
                 </div>
-                <div className="flex flex-row gap-3 justify-between">
+                <div className="flex flex-col lg:flex-row gap-3 justify-between">
                     <div className="flex space-x-7 items-center h-[150px]">
                             <a 
                                 href="/Tusher Debnath.pdf"
                                 download='Tusher Debnath.pdf'
                             >
-                                    <button className="flex gap-2 px-4 py-2 h-10 border-2 border-blue-500 text-blue-500 rounded transition duration-300 hover:bg-red-500 hover:text-white transform hover:scale-105">
+                                    <button className="flex gap-2 px-4 py-2 h-10 ml-28 lg:ml-0 border-2 border-blue-500 text-blue-500 rounded transition duration-300 hover:bg-red-500 hover:text-white transform hover:scale-105">
                                 <p>Resume</p>
                                 <img className="bg-emerald-50 h-[25px]" src={resumeDown} alt="" /> 
                             </button>
                             </a>
+                            <div className='flex gap-7 mt-5 lg:mt-0'>
                             <a href="#project">
-                                <button class="px-4 py-2 h-10 border-2 border-blue-500 text-blue-500 rounded transition duration-300 hover:bg-blue-500 hover:text-white">
+                                <button class="hidden lg:block px-4 py-2 h-10 border-2 border-blue-500 text-blue-500 rounded transition duration-300 hover:bg-blue-500 hover:text-white">
                                 View My Work
                             </button>
                             </a>
                             <a href="#contract">
-                                <button class="px-4 py-2 h-10 border-2 border-blue-500 text-blue-500 rounded transition duration-300 hover:bg-blue-500 hover:text-white">
+                                <button class="hidden lg:block px-4 py-2 h-10 border-2 border-blue-500 text-blue-500 rounded transition duration-300 hover:bg-blue-500 hover:text-white">
                                 Contact Me
                             </button>
                             </a>
+                            </div>
                     </div>                    
-                    <div className="card-wrapper h-[150px] w-[600px]">
+                    <div className="card-wrapper h-[150px] w-[370px] lg:w-[600px]">
                         <div className="card-content flex items-center justify-between text-xs">
-                            <img className="h-[150px] w-[200px]" src={location} alt="" />
+                            <img className="h-[150px] w-[150px] lg:w-[200px]" src={location} alt="" />
                             {/* <p className="text-2xl pl-5">Baised in:</p> */}
-                            <div className="w-[400px] ml-5">
+                            <div className="w-[400px] ml-3 lg:ml-5">
                                 <p className="text-2xl">Baised in:</p>
                                 <div className="flex flex-row gap-1">
-                                    <p className="text-2xl pt-5">Dhaka, Bangladesh</p>
-                                    <img className="h-[80px] w-[80px] ml-5 rounded-full" src={bdFlag} alt="" />
+                                    <p className="text-xl lg:text-2xl pt-5">Dhaka, Bangladesh</p>
+                                    <img className="h-[50px] lg:h-[80px] w-[50px] lg:w-[80px] mt-7 lg:mt-0 mr-3 lg:ml-5 rounded-full" src={bdFlag} alt="" />
                                 </div>
                             </div>
-
                         </div>
                     </div> 
                 </div>        
